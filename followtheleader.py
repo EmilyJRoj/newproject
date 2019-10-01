fdee8c257662efa1a50cc5f0f64fb5fb57b8bf4b
turtleartgallery <100157892@lubbockisd.net>
2019-10-01 08:55:03 -0500

    Initial commit

---
+++ followtheleader.py
@@ -0,0 +1,22 @@
+# testing out messages
+print("hello world")
+print("Howdy y'all")
+print("I like typing this.")
+print("This is fun.")
+
+# trying out some maths with chickens
+print("I will now count my chickens", 2)
+print("Hens: ", 25+30/6)
+print("Roosters: ", 100 - 25 * 3 % 4)
+
+# trying out more maths with eggs.
+print("Now i will count the eggs", 7)
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print(3 + 2< 5 - 7)
+
+# make more math problems
+print(5 + 3 + 2 - 5 % 6 / 6 + 8)
+# fixing the math
+print(5.0 + 3.3 + 2.0 - 5.0 % 6.7 / 6.0 + 8.0)
+
+
