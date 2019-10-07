d2fce17e8a5d423722ddfa310b46b7607bc70fe5
turtleartgallery <100157892@lubbockisd.net>
2019-10-07 08:44:53 -0500

    pushing first version of today's work. plus now with #'s.

--- newproject.py
+++ newproject.py
@@ -1,18 +1,23 @@
+# creating the situation
 x = "There are %d types of people." %10
 binary = "binary"
 doNot = "don't"
 y = "those who know %s and those who %s." % (binary, doNot)
 
+# adding x's and y's
 print(x)
 print(y)
 
+# what I said...
 print("I said: %r" % x )
 print("I also said: '%s'." % y )
 
+# is the joke is funny or not?
 hilarious = False
 joke_evaluation = "Isn't that joke so funny?! %r"
 print(joke_evaluation % hilarious)
 
+# string with w and e...
 w = "This is the left side of..."
 e = "a string with a right side."
 
